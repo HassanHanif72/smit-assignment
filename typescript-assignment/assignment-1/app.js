5915d8bc047391b17f9ff7b72b7014907e14cda9
hassanhanif72 <hassanhanif72@gmail.com>
2025-01-14 23:29:32 +0500

    Typescript Assignment

---
+++ typescript-assignment/assignment-1/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let userName = "Hassan";
+let password = 1234;
+document.write(`User Name: ${userName} <br>`);
+document.write(`Password: ${password}`);
---
+++ typescript-assignment/assignment-1/app.ts
@@ -0,0 +1,5 @@
+let userName: string = "Hassan"
+let password: number = 1234
+
+document.write(`User Name: ${userName} <br>`);
+document.write(`Password: ${password}`);
